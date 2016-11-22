7152d2f4040e6f771a1ab1fb645f58e1f4a26d02
Khang Nguyen <khang.neon.1997@gmail.com>
2016-11-22 21:30:08 +0700

    [REFACTOR] Move ProductsActivity to `activity` package

--- app/src/main/java/com/github/khangnt/transactionviewer/activity/ProductsActivity.java
+++ app/src/main/java/com/github/khangnt/transactionviewer/activity/ProductsActivity.java
@@ -1,7 +1,9 @@
-package com.github.khangnt.transactionviewer;
+package com.github.khangnt.transactionviewer.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.github.khangnt.transactionviewer.R;
 
 public class ProductsActivity extends AppCompatActivity {
 
